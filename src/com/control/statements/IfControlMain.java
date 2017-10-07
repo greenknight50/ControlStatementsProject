76d4b4683b9e96218b83e15821b5071c3e137d95
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-10-07 08:19:53 +0530

    Initial commit

---
+++ src/com/control/statements/IfControlMain.java
@@ -0,0 +1,31 @@
+package com.control.statements;
+
+public class IfControlMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Inside main");
+		int a = 5, b = 10, c = 15;
+		IfControlMain ifControlMain = new IfControlMain();
+
+		if (c < a + b) {
+			// get user registered
+			System.out.println("C is less than a +b");
+		} else if (c > a+b) {
+			System.out.println("C is greater than a + b");
+		} else {
+			System.out.println("C is equal to a+b");
+		}
+		// System.out.println("C: " + c);
+		System.out.println("Outside if");
+
+	}
+
+	public boolean isUserRegistered() {
+		boolean returnValue = false;
+
+		return returnValue;
+
+	}
+
+}
