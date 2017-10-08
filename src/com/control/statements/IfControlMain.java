07965c591b1578baed0e6d90c153e80a59a25060
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-07 22:01:08 -0500

    Descending

--- src/com/control/statements/IfControlMain.java
+++ src/com/control/statements/IfControlMain.java
@@ -4,9 +4,9 @@ public class IfControlMain {
 
 		System.out.println("Inside main");
 		int a = 5, b = 10, c = 15;
-		if(a < b && a < c){
+		if(a > b && a > c){
 			System.out.println(a);
-		if(b < c){
+		if(b > c){
 			System.out.println(b);
 			System.out.println(c);
 			}
@@ -15,9 +15,9 @@ public class IfControlMain {
 			System.out.println(b);
 			}
 			}
-		else if(b < a && b < c){
+		else if(b > a && b > c){
 			System.out.println(b);
-		if( a < c){
+		if( a > c){
 			System.out.println(a);
 			System.out.println(c);
 			}
@@ -28,14 +28,14 @@ public class IfControlMain {
 			}
 		else{
 			System.out.println(c);
-		if( a < b){
+		if( a > b){
 			System.out.println(a);
 			System.out.println(b);
 			}
 		else{
 			System.out.println(b);
 			System.out.println(a);
-			}
+			}		
 				}
 		System.out.println("Outside if");
 	}
