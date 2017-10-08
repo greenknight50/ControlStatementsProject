c9f86ea13fa933d6d4d718e56a6ab4ac45d28168
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-07 21:09:37 -0500

    Ascending

--- src/com/control/statements/IfControlMain.java
+++ src/com/control/statements/IfControlMain.java
@@ -1,20 +1,42 @@
-package com.control.statements;
-
 public class IfControlMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Inside main");
 		int a = 5, b = 10, c = 15;
-		if (c < a + b) {
-			// get user registered
-			System.out.println("C is less than a +b");
-		} else if (c > a + b) {
-			System.out.println("C is greater than a + b");
-		} else {
-			System.out.println("C is equal to a+b");
-		}
-		// System.out.println("C: " + c);
+		if(a < b && a < c){
+			System.out.println(a);
+		if(b < c){
+			System.out.println(b);
+			System.out.println(c);
+			}
+		else{
+			System.out.println(c);
+			System.out.println(b);
+			}
+			}
+		else if(b < a && b < c){
+			System.out.println(b);
+		if( a < c){
+			System.out.println(a);
+			System.out.println(c);
+			}
+		else{
+			System.out.println(c);
+			System.out.println(a);
+			}
+			}
+		else{
+			System.out.println(c);
+		if( a < b){
+			System.out.println(a);
+			System.out.println(b);
+			}
+		else{
+			System.out.println(b);
+			System.out.println(a);
+			}
+				}
 		System.out.println("Outside if");
 	}
 
