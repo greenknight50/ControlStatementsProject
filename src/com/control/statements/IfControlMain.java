d5b6931693fd979150f3d3fd1fac69f06e9a75d1
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-10-11 01:26:00 +0530

    Array declaration, initialization

--- src/com/control/statements/IfControlMain.java
+++ src/com/control/statements/IfControlMain.java
@@ -1,43 +1,39 @@
+package com.control.statements;
 public class IfControlMain {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-
-		System.out.println("Inside main");
-		int a = 5, b = 10, c = 15;
-		if(a > b && a > c){
-			System.out.println(a);
-		if(b > c){
-			System.out.println(b);
-			System.out.println(c);
-			}
-		else{
-			System.out.println(c);
-			System.out.println(b);
-			}
-			}
-		else if(b > a && b > c){
-			System.out.println(b);
-		if( a > c){
-			System.out.println(a);
-			System.out.println(c);
-			}
-		else{
-			System.out.println(c);
-			System.out.println(a);
-			}
-			}
-		else{
-			System.out.println(c);
-		if( a > b){
-			System.out.println(a);
-			System.out.println(b);
-			}
-		else{
-			System.out.println(b);
-			System.out.println(a);
-			}		
-				}
-		System.out.println("Outside if");
+		
+		int arr[] = {82,83,12,92,72};
+		int arr1[] = new int[6];
+		int[] arr3 = getArray();
+		
+		System.out.println("arr1[4]: " +arr1[4]);
+		System.out.println("arr[2]: " + arr[2]);
+		System.out.println("arr length: " +arr.length);
+		System.out.println("arr1 length: " +arr1.length);
+		System.out.println("arr3 length: " +arr3.length);
+		
+		for (int i=0; i<arr.length; i++) {
+			System.out.println("arr["+i+"]: "+arr[i]);
+		}
+		
+		System.out.println("Array elements using for-each loop");
+		//int i;
+		for (int i : arr) {
+			System.out.println("arr["+i+"]: "+i);
+		}
+		
+	}
+	
+	
+	public static int[] getArray() {
+		
+		//database access
+		int arr[] = {23, 56, 73};
+		return arr ;
 	}
 
 }
