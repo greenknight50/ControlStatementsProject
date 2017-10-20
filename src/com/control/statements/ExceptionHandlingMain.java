907410ec9eb89161e3faf566e75e37e6cb770796
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-10-20 21:53:59 +0530

    Exception Handling superclass-subclass exception throwing

--- src/com/control/statements/ExceptionHandlingMain.java
+++ src/com/control/statements/ExceptionHandlingMain.java
@@ -1,38 +1,24 @@
 package com.control.statements;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class ExceptionHandlingMain {
 
-	public static void main(String[] args) {
-		Scanner scanner;
-		scanner = new Scanner(System.in);
-		int x, y, z = 0;
-		ExceptionHandlingMain obj = new ExceptionHandlingMain();
-		System.out.println("Inside Main method");
-		try {
-			Class cls = Class.forName("com.control.statements.ExceptionHandlingMain");
-			ExceptionHandlingMain obj1 = (ExceptionHandlingMain) cls.newInstance();
-			System.out.println("obj1: " + obj1);
-			System.out.println("Please enter x value: ");
-			x = scanner.nextInt();
-			System.out.println("Please enter y value: ");
-			y = scanner.nextInt();
-			z = x / y;
-			
-		} catch (ClassNotFoundException | ArithmeticException ex) {
-			System.out.println("Common catch block");
-			z = 35;
-			System.out.println("Class exception: " + ex.getMessage());
-		} catch (Exception e) {
-			z = 20;
-			e.printStackTrace();
-		} finally {
-			scanner.close();
-			System.out.println("This will be executed all time unless system crashes");
-		}
-		System.out.println("Z value: " + z);
+class DemoException {
+	
+	void displaySomething() throws IOException {
+		System.out.println(5/0);
+	}
+}
 
+public class ExceptionHandlingMain extends DemoException {
+
+	public static void main(String[] args) {}
+	
+	@Override
+	void displaySomething() throws FileNotFoundException {
+		
 	}
 
 }
