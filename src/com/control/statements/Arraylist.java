2ae795f58385d7190ad8d474f175d2f9d19df3b1
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-24 10:35:35 -0500

    Arraylist Example

---
+++ src/com/control/statements/Arraylist.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Arraylist {
+   public static void main(String args[]) {
+      
+	  ArrayList<String> obj = new ArrayList<String>();
+
+	  obj.add("Highland");
+	  obj.add("Leavit");
+	  obj.add("bell");
+	  obj.add("Oakely");
+	  obj.add("Claremont");
+
+	  System.out.println("Array list has following elements:"+obj);
+          System.out.println(obj.indexOf("bell"));
+          System.out.println(obj.get(4));
+
+	  obj.add(0, "Rosemont");
+	  obj.add(1, "Thome");
+
+	  obj.remove("bell");
+	  obj.remove("Leavit");
+
+	  System.out.println("Current array list is:"+obj);
+
+	  obj.remove(1);
+
+	  System.out.println("Current array list is:"+obj);
+   }
+}
