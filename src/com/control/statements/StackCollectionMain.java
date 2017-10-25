b885254b68bb5cc6cb298e2bee3915c2b89d8ff5
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-25 14:38:16 -0500

    stack example using strings

--- src/com/control/statements/StackCollectionMain.java
+++ src/com/control/statements/StackCollectionMain.java
@@ -1,5 +1,3 @@
-package com.control.statements;
-
 import java.util.Stack;
 
 public class StackCollectionMain {
@@ -7,20 +5,18 @@ public class StackCollectionMain {
 	public static void main(String[] args) {
 		Stack<Integer> stack = new Stack<Integer>();
 		System.out.println("Is stack empty: " + stack.empty() + ", Stack capacity: " + stack.capacity() + ", size: " + stack.size());
-		stack.push(83);
-		stack.push(29);
-		stack.push(30);
-		stack.add(1, 54);
-		stack.add(63);
+		stack.push(Leavit);
+		stack.push(Bell);
+		stack.push(Oakley);
+		stack.add(1, Thome);
+		stack.add(Rosemont);
 		System.out.println("Stack before replacing with set: " + stack);
-		stack.set(2, 19);
-		stack.setElementAt(67, 0);
-		stack.insertElementAt(105, 5);
-		stack.add(34);
-		stack.add(23);
-		stack.add(53);
-		stack.add(36);
-		stack.add(89);
+		stack.set(2, Highland);
+		stack.add(Western);
+		stack.add(Artesian);
+		stack.add(Maplewood);
+		stack.add(Rockwell);
+		stack.add(Downtown);
 		System.out.println("\nIs stack empty: " + stack.empty() + ", Elements of stack are: " + stack);
 		System.out.println("Popped element: " + stack.pop());
 		System.out.println("Popped element: " + stack.pop());
