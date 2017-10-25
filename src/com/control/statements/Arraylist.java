861ce4c013544b267626d1a7a72b3a64efd002f8
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-25 14:54:11 -0500

    Arraylist new example

--- src/com/control/statements/Arraylist.java
+++ src/com/control/statements/Arraylist.java
@@ -1,33 +1,24 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class Arraylist{
+ 
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(10);
+        numbers.add(100);
+        numbers.add(40);
+         System.out.println(numbers.get(0));
+        System.out.println("Case #1: ");      
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }	
+        numbers.remove(numbers.size() - 1);
+        numbers.remove(0);
+        System.out.println("Case #2: ");
+        for (Integer value : numbers) {
+            System.out.println(value);
+        }
 
-public class Arraylist {
-   public static void main(String args[]) {
-      
-	  ArrayList<String> obj = new ArrayList<String>();
-
-	  obj.add("Highland");
-	  obj.add("Leavit");
-	  obj.add("bell");
-	  obj.add("Oakely");
-	  obj.add("Claremont");
-          System.out.println("Size Of ArrayList = "+obj.size());
-	  System.out.println("Array list has following elements:"+obj);
-          System.out.println(obj.indexOf("bell"));
-          System.out.println(obj.get(4));
-          List<String> subList = obj.subList(1, 4);
-
-	  obj.add(0, "Rosemont");
-	  obj.add(1, "Thome");
-
-	  obj.remove("bell");
-	  obj.remove("Leavit");
-
-	  System.out.println("Current array list is:"+obj);
-
-	  obj.remove(1);
-
-	  System.out.println("Current array list is:"+obj);
-   }
+    }
 }
-
-
