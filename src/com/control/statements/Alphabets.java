dac75504aa9ed39187769685f2bd5bfd070ad31c
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-25 15:10:10 -0500

    List to stack example

--- src/com/control/statements/Alphabets.java
+++ src/com/control/statements/Alphabets.java
@@ -1,10 +1,15 @@
-package com.control.statements;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Alphabets {
-
-	public static void main(String args[]){
-	int i;
-	for(i = 1 ; i <= 10 ; i++)
-	System.out.println(i);
-	}
+    public static void main(String a[]){
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(100);
+        list.add(200);
+        list.add(300);
+        System.out.println("Is stack empty : "  + stack.addAll(list));
+        System.out.println("Empty stack : "  + stack);
+    }
 }
