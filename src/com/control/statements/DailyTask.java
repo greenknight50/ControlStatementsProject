048b2a4dcc22fddd662696c813ab56c7d9280988
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-25 19:00:33 -0500

    Arraylist with Geneics

--- src/com/control/statements/DailyTask.java
+++ src/com/control/statements/DailyTask.java
@@ -1,14 +1,37 @@
-abstract class DailyTask{
-   
-   public abstract void thisweek();
+import java.util.ArrayList;
+import java.util.HashMap;
+ 
+class Animal {
+     
 }
-public class Today extends DailyTask{
-
-   public void thisweek(){
-	System.out.println("Wednesday");
-   }
-   public static void main(String args[]){
-	DailyTask obj = new Today ();
-	obj.thisweek();
-   }
+ 
+ 
+public class DailyTask {
+ 
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+         
+        list.add("apple");
+        list.add("banana");
+        list.add("orange");
+         
+        String fruit = (String)list.get(1);
+         
+        System.out.println(fruit);
+         
+        ArrayList<String> strings = new ArrayList<String>();
+         
+        strings.add("cat");
+        strings.add("dog");
+        strings.add("alligator");
+         
+        String animal = strings.get(1);
+         
+        System.out.println(animal);
+         
+        HashMap<Integer, String> map = new HashMap<Integer, String>()
+         
+        ArrayList<Animal> someList = new ArrayList<>();
+    }
+ 
 }
