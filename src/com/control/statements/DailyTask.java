79c5dfb75ee7e805f3da7316b54b769719966813
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-31 15:42:19 -0500

    Hashlinkedset

--- src/com/control/statements/DailyTask.java
+++ src/com/control/statements/DailyTask.java
@@ -1,37 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
- 
-class Animal {
-     
-}
- 
- 
-public class DailyTask {
- 
-    public static void main(String[] args) {
-        ArrayList list = new ArrayList();
-         
-        list.add("apple");
-        list.add("banana");
-        list.add("orange");
-         
-        String fruit = (String)list.get(1);
-         
-        System.out.println(fruit);
-         
-        ArrayList<String> strings = new ArrayList<String>();
-         
-        strings.add("cat");
-        strings.add("dog");
-        strings.add("alligator");
-         
-        String animal = strings.get(1);
-         
-        System.out.println(animal);
-         
-        HashMap<Integer, String> map = new HashMap<Integer, String>()
-         
-        ArrayList<Animal> someList = new ArrayList<>();
-    }
- 
+import java.util.*;
+public class HashSetDemo {
+
+   public static void main(String args[]) {
+     LinkedHashSet hash = new LinkedHashSet();
+      hash.add("Bell");
+      hash.add("Leavit");
+      hash.add("Oakley");
+      hash.add("devon");
+      hash.add("Thome");
+      hash.add("Granville");
+      System.out.println(hash);
+   }
 }
