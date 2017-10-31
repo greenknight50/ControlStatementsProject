7aa8bbc845874b3cd9ee50a1ec6e7223b6d2ff65
Wahid Ali <wahidalimohammed@outlook.com>
2017-10-31 15:52:10 -0500

    hashlinkedset using interger

--- src/com/control/statements/DailyTask.java
+++ src/com/control/statements/DailyTask.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class HashSetDemo {
+public class HashSDemo {
 
    public static void main(String args[]) {
      LinkedHashSet hash = new LinkedHashSet();
@@ -10,5 +10,13 @@ public class HashSetDemo {
       hash.add("Thome");
       hash.add("Granville");
       System.out.println(hash);
+
+	LinkedHashSet<Integer> hash1 = new LinkedHashSet<Interger>();
+	hash1.add(000);
+	hash1.add(111);
+	hash1.add(222);
+	hash1.add(333);
+	hash1.add(444);
+	system.out.println(hash1);
    }
 }
