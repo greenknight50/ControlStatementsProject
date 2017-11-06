5ee7b6d1125f40052cd28b605ff5c33c2eac4f5b
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-11-06 23:03:51 +0530

    Threads program

--- src/com/control/statements/ThreadClassMain.java
+++ src/com/control/statements/ThreadClassMain.java
@@ -5,33 +5,28 @@ public class ThreadClassMain extends Thread {
 	@Override
 	public void run() {
 		System.out.println("Thread Name: " + this.getName() + ", isAlive: " + this.isAlive());
-		SecondThreadMain secondT = new SecondThreadMain();
-		secondT.setName("SecondThreadMain");
-		secondT.start();
-		try {
-			System.out.println("about to join t1 to the end of secondT and thread name is: " + this.getName());
-			secondT.join(); 
-			System.out.println("After join call");
-			for (int i = 0; i < 10; i++) {
-				System.out.println("Run method of thread: " + this.getName() + ", with i: " + i);
+		for (int i = 0; i < 2000; i++) {
+			System.out.println("Run method of thread: " + this.getName() + ", with i: " + i);
+			try {
 				this.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		ThreadClassMain t1 = new ThreadClassMain();
 		t1.setName("FirstThread");
+		t1.setPriority(Thread.MAX_PRIORITY);
 		ThreadClassMain t2 = new ThreadClassMain();
 		t2.setName("SecondThread");
-		t1.run();
-		Thread.sleep(500);
+		t2.setPriority(Thread.MIN_PRIORITY);
+		System.out.println("FirstThread priority: " + t1.getPriority());
+		System.out.println("SecondThread priority: " + t2.getPriority());
 		t2.start();
-		t1.join();
-		t2.join();
+		t1.start();
+		t1.yield();
 		System.out.println("End of Main method");
 	}
-
 }
