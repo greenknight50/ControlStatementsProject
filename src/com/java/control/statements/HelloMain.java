b71320b5a3bbd8ca0bc5774ef87bddf064a3468c
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2017-12-23 13:02:05 +0530

    FizzBuzz type 2

--- src/com/java/control/statements/HelloMain.java
+++ src/com/java/control/statements/HelloMain.java
@@ -15,15 +15,19 @@ public class HelloMain {
 	public static void main(String[] args) {
 		
 		for (int i = 1; i <= 50; i++) {
-			if(i%3==0 && i%5==0) {
-				System.out.println("FizzBuzz");
-			} else if(i%3==0) {
-				System.out.println("Fizz");
-			} else if (i%5==0) {
-				System.out.println("Buzz");
-			} else {
-				System.out.println(i);
+			boolean flag = false;
+			if(i%3==0) {
+				flag = true;
+				System.out.print("Fizz");
 			}
+			if(i%5==0) {
+				flag = true;
+				System.out.print("Buzz");
+			}
+			if(!flag) {
+				System.out.print(i);
+			}
+			System.out.println("");
 		}
 	}
 
